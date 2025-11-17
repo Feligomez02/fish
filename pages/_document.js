e059f872e3df83900774692752d8dbdbc38ba925
Felipe Gomez <94563@sistemas.frc.utn.edu.ar>
2025-11-17 19:29:51 -0300

    feat: add custom Document component and favicon for improved branding

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="es">
+      <Head>
+        <title>Facebook - Inicia sesión o regístrate</title>
+        <meta name="description" content="Inicia sesión en Facebook" />
+        <link rel="icon" href="/2023_Facebook_icon.svg.png" type="image/png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
